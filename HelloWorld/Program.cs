9133c90f0621d4093492b64e01af9ead93b6f282
Anderson Araujo <andersonaraujo@users.noreply.github.com>
2016-10-18 10:59:24 -0200

    Update Program.cs
    testing line count

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,20 +1,16 @@
 ﻿// A Hello World! program in C#.
 using System;
-namespace HelloWorld
-{
-    public class Hello
-    {
-        static void Main()
-        {
+namespace HelloWorld {
+    public class Hello {
+        static void Main() {
             Console.WriteLine("Hello World!");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
-
-        public int getInt()
-        {
+        
+        public int getInt() {
             return 1;
         }
     }
